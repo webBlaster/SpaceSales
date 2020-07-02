62db90fef6a04656daed58124c6ace3ad2b429e1
Yekinni Ajibola <yekinnijibola@gmail.com>
2020-07-03 00:24:34 +0100

    updated config

--- Inc/Config.php
+++ Inc/Config.php
@@ -4,9 +4,9 @@ define("BASE_URL","/");
 define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"]."/");
 
 //absolute server paths throughout all the code
-define("DB_HOST","localhost");
-define("DB_NAME","spacesales");
+define("DB_HOST","us-cdbr-east-02.cleardb.com");
+define("DB_NAME","heroku_143e51e34d52c49");
 define("DB_PORT","3306");//default:3306
-define("DB_USER","root");
-define("DB_PASS","");
+define("DB_USER","bd88c15a35fe11");
+define("DB_PASS","792cc21d");
 ?>
